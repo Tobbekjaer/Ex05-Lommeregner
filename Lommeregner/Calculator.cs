2b644298aa2192f3f73c2b1097ef4559068e40d7
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-03-30 11:36:37 +0200

    Lommeregner projekt

---
+++ Lommeregner/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lommeregner
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            Console.WriteLine("Du tastede 1: ");
+            int add = num1 + num2;
+            Console.WriteLine("The addition of " + num1 + num2 + " is: " + add);
+            return add;
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            Console.WriteLine("Du tastede 2: ");
+            int subtract = num1 - num2;
+            Console.WriteLine("The subtraction of " + num1 + num2 + " is: " + subtract);
+            return subtract;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            Console.WriteLine("Du tastede 3: ");
+            double divide = num1 / num2;
+            Console.WriteLine("The division of " + num1 + num2 + " is: " + divide);
+            return divide;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            Console.WriteLine("Du tastede 4: ");
+            int multiply = num1 * num2;
+            Console.WriteLine("The multiplication of " + num1 + num2 + " is: " + multiply);
+            return multiply;
+        }
+    }
+}
---
+++ Lommeregner/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel.Design;
+
+namespace Lommeregner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Menu();
+
+            
+
+
+
+            Console.ReadLine();
+        }
+
+
+        
+        static void Menu()
+        {
+
+        
+
+            Console.Clear();
+
+            
+
+            Console.WriteLine(
+    @"Min lommeregner: 
+1. Brug addition
+2. Brug subtraktion
+3. Brug division
+4. Brug multiplikation
+5. Luk lommeregneren...");
+
+            // Creating a new instance of the Calculator class
+            Calculator calc = new Calculator();
+
+            // Jeg erklærer to variable
+            int num1 = 8;
+            int num2 = 4;
+            var key = Console.ReadKey(true); // Jeg laver et nyt key instance, som skal være true, så det læser tastaturtrykket med det samme
+
+            // Opretter et switch statement til at oprette forskellige udfald
+            switch (key.Key) // Vi bruger vores variabel key med .Key metoden, som kan læse hvilken tast, der bliver trykket på
+            {
+                case ConsoleKey.D1: // Læser tasten 1 fra tastaturet
+                    int add = calc.Add(num1, num2); // Bruger calc.Add() metoden fra Calculator.cs
+                    Console.ReadLine(); 
+                    Menu();
+                    break;
+                case ConsoleKey.D2:
+                    int subtract = calc.Subtract(num1, num2); 
+                    Console.ReadLine();
+                    Menu();
+                    break;
+                case ConsoleKey.D3:
+                    double divide = calc.Divide(num1, num2); 
+                    Console.ReadLine();
+                    Menu();
+                    break;
+                case ConsoleKey.D4:
+                    int multiply = calc.Multiply(num1, num2); 
+                    Console.ReadLine();
+                    Menu();
+                    break;
+                case ConsoleKey.D5:
+                    Console.WriteLine("Du tastede: 5");
+                    Console.WriteLine("Du valgte at lukke lommeregneren...");
+                    break;
+                default:
+                    Console.WriteLine("Ugyldigt input: vælg igen...");
+                    Console.ReadLine();
+                    Menu();
+                    break;
+                    
+
+            }
+
+
+
+
+        }
+    }
+}
